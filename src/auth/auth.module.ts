472514368e60f67c7f2ffc849f57792f32129623
taimurwaheed <taimurwaheed34570@gmail.com>
2026-04-25 16:15:53 +0500

    feat: add all NestJS modules structure

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtStrategy } from './jwt.strategy';
+import { SeedService } from './seed.service';
+import { User } from '../entities/user.entity';
+import { LeaveBalance } from '../entities/leave-balance.entity';
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([User, LeaveBalance]),
+        PassportModule,
+        JwtModule.register({
+            secret: 'timeoff_secret_key',
+            signOptions: { expiresIn: '24h' },
+        }),
+    ],
+    controllers: [AuthController],
+    providers: [AuthService, JwtStrategy, SeedService],
+    exports: [AuthService, JwtModule],
+})
+export class AuthModule { }
